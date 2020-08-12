9b90cd40b2cd5f9fb472961263284d8a763f185a
ammaraslam10 <40186412+ammaraslam10@users.noreply.github.com>
2020-08-12 21:25:55 +0500

    Update JTiledUtility.java

--- JTiledUtility.java
+++ JTiledUtility.java
@@ -7,17 +7,22 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 public class JTiledUtility {
     JGameEngine e;
     ArrayList<JGameEngine.Object> objects;
+    
+    String em[] = new String[4];
     public JTiledUtility(JGameEngine e) {
 	this.e = e;
 	objects = new ArrayList<>();
     }
-    public <T extends JGameEngine.Object> void setGameSpace(String filename, ArrayList<Class> clas) {
+    public <T extends JGameEngine.Object> ArrayList<ArrayList<JGameEngine.Object>> setGameSpace(String filename, ArrayList<Class> clas) {
 	Gson gson = new Gson();
 	JsonObject object = null; 
 	try {
@@ -37,7 +42,8 @@ public class JTiledUtility {
 		JTiledDraw draw = new JTiledDraw(e);
 		addSprites(draw, directory, object.get("tilesets").getAsJsonArray().get(0).getAsJsonObject().get("source").getAsString());
 		draw.mapwidth = width;
-		e.addObject(draw);
+		e.objectAdd(draw);
+		ArrayList<ArrayList<JGameEngine.Object>> objects = new ArrayList<>();
 		int objectgroupcount = 0;
 		for(int i = 0; i < layers.size(); i++) {
 		    JsonObject layer = layers.get(i).getAsJsonObject();
@@ -47,15 +53,19 @@ public class JTiledUtility {
 			else 
 			    System.out.println("JTiledUtility::setGameSpace() unsupported tiled layer, skipped.");
 		    } else if(layer.get("type").getAsString().equals("objectgroup")) {
+			ArrayList<JGameEngine.Object> layer_objects = new ArrayList<>();
 			if(clas == null)
-			    solveObjects(layer, JTiledObject.class);
+			    solveObjects(layer, JTiledObject.class, layer_objects);
 			else
-			    solveObjects(layer, clas.get(objectgroupcount));
+			    solveObjects(layer, clas.get(objectgroupcount), layer_objects);
 			objectgroupcount++;
+			objects.add(layer_objects);
 		    }
 		}
+		return objects;
 	    } else { System.out.println("JTiledUtility::setGameSpace() unsupported tiled format."); }
 	} catch(FileNotFoundException e) { System.out.println("JTiledUtility::setGameSpace() file not found."); }	
+	return null;
     }
     private void addSprites(JTiledDraw draw, String directory, String filename) {
 	BufferedImage tmp = null;
@@ -82,33 +92,60 @@ public class JTiledUtility {
     public void setGameSpace(String filename) {
 	setGameSpace(filename, null);
     }
-    private <T extends JGameEngine.Object> void solveObjects(JsonObject layer, Class<T> clas) {
+    private <T extends JGameEngine.Object> void solveObjects(JsonObject layer, Class<T> clas, ArrayList<JGameEngine.Object> objects) {
 	JsonArray jobjarr = layer.get("objects").getAsJsonArray();
 	if(clas == null) clas = (Class<T>) JTiledObject.class;
+	Constructor[] ctors = clas.getDeclaredConstructors();
+	Constructor ctor = null;
+	for (int i = 0; i < ctors.length; i++) {
+	    ctor = ctors[i];
+	    if (ctor.getGenericParameterTypes().length == 1)
+		break;
+	}
+	if(ctor == null) { 
+	    System.out.println("JTiledUtility::setGameSpace() Class needs to have a constructor that only has 1 argument that accepts a JGameEngine ("+clas.getName()+")");
+	    System.exit(0);
+	}
+	T standardObj = null;
 	try {
-	    Constructor[] ctors = clas.getDeclaredConstructors();
-	    Constructor ctor = null;
-	    for (int i = 0; i < ctors.length; i++) {
-		ctor = ctors[i];
-		if (ctor.getGenericParameterTypes().length == 1)
-		    break;
-	    }
-	    if(ctor == null) System.out.println("JTiledUtility::setGameSpace() Class needs to have a constructor that only has 1 argument that accepts a JGameEngine");
-	    T standardObj = (T) ctor.newInstance(e);
-	    int maskCount = 0;
-	    for(int i = 0; i < jobjarr.size(); i++) {
-		JsonObject jobj = jobjarr.get(i).getAsJsonObject();
-		if(jobj.get("name").getAsString().equals("")) {
-		    maskCount += addObjectMask(standardObj, jobj);
-		} else {
-		    JTiledObject newObj = new JTiledObject(e);
-		    addObjectMask(newObj, jobj);
-		    newObj.name = jobj.get("name").getAsString();
-		    e.addObject(newObj);
+	    standardObj = (T) ctor.newInstance(e);
+	} catch (InstantiationException ex) {
+	    System.out.println("JTiledUtility::setGameSpace() Can't Instantiate object (Class "+clas.getName()+") (object creation)"); Logger.getLogger(JTiledUtility.class.getName()).log(Level.SEVERE, null, ex);
+	} catch (IllegalAccessException ex) {
+	    System.out.println("JTiledUtility::setGameSpace() Can't Access (Class \""+clas.getName()+"\" needs to have a PUBLIC constructor that accepts a JGameEngine object)"); Logger.getLogger(JTiledUtility.class.getName()).log(Level.SEVERE, null, ex);
+	} catch (IllegalArgumentException ex) {
+	    System.out.println("JTiledUtility::setGameSpace() Invalid Argument to Constructor (Class \""+clas.getName()+"\" needs to have a constructor that only has 1 argument that accepts a JGameEngine)"); Logger.getLogger(JTiledUtility.class.getName()).log(Level.SEVERE, null, ex);
+	} catch (InvocationTargetException ex) {
+	    System.out.println("JTiledUtility::setGameSpace() Invocation Target Exception (Class "+clas.getName()+") (object creation)"); Logger.getLogger(JTiledUtility.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	int maskCount = 0;
+	for(int i = 0; i < jobjarr.size(); i++) {
+	    JsonObject jobj = jobjarr.get(i).getAsJsonObject();
+	    if(jobj.get("name").getAsString().equals("")) {
+		maskCount += addObjectMask(standardObj, jobj);
+	    } else {
+		T newObj = null;
+		try {
+		    newObj = (T) ctor.newInstance(e);
+		} catch (InstantiationException ex) {
+		    System.out.println("JTiledUtility::setGameSpace() Can't Instantiate object (Class "+clas.getName()+") (object creation)"); Logger.getLogger(JTiledUtility.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+		    System.out.println("JTiledUtility::setGameSpace() Can't Access (Class \""+clas.getName()+"\" needs to have a PUBLIC constructor that accepts a JGameEngine object)"); Logger.getLogger(JTiledUtility.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (IllegalArgumentException ex) {
+		    System.out.println("JTiledUtility::setGameSpace() Invalid Argument to Constructor (Class \""+clas.getName()+"\" needs to have a constructor that only has 1 argument that accepts a JGameEngine)"); Logger.getLogger(JTiledUtility.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (InvocationTargetException ex) {
+		    System.out.println("JTiledUtility::setGameSpace() Invocation Target Exception (Class "+clas.getName()+") (object creation)"); Logger.getLogger(JTiledUtility.class.getName()).log(Level.SEVERE, null, ex);
 		}
+		addObjectMask(newObj, jobj);
+		newObj.name = jobj.get("name").getAsString();
+		e.objectAdd(newObj);
+		objects.add(newObj);
 	    }
-	    if(maskCount != 0) e.addObject(standardObj);
-	} catch(Exception e) { System.out.println("JTiledUtility::setGameSpace() invalid object class, skipping " + e.toString()); e.printStackTrace(); }
+	}
+	if(maskCount != 0)  {
+	    e.objectAdd(standardObj);
+	    objects.add(standardObj);
+	}
     }
     private <T extends JGameEngine.Object> int addObjectMask(T obj, JsonObject jobj) {
 	int maskCount = 0;
@@ -152,10 +189,10 @@ class JTiledDraw extends JGameEngine.Object {
 		    int num = tox + j1 * mapwidth + mapwidth * toy + k1;
 		    if(num >= 0 && num < layer.size() && layer.get(num).getAsInt() > 0) {
 			e.draw().drawImage(img[layer.get(num).getAsInt() - 1], 
-			(int) (((k * tile_width) - (e.cameraX() % tile_width)) * 1/e.cameraDistance()),// + (int) ((e.cameraX()/tile_width)*tile_width), 
-			(int) (((j * tile_height) - (e.cameraY() % tile_height))* 1/e.cameraDistance()),// + (int) ((e.cameraY()/tile_height)*tile_height),
-			(int) (tile_width * 1/e.cameraDistance()), 
-			(int) (tile_height * 1/e.cameraDistance()), null);	
+			(int) Math.round(((k * tile_width) - (e.cameraX() % tile_width)) * 1/e.cameraDistance()),// + (int) ((e.cameraX()/tile_width)*tile_width), 
+			(int) Math.round(((j * tile_height) - (e.cameraY() % tile_height))* 1/e.cameraDistance()),// + (int) ((e.cameraY()/tile_height)*tile_height),
+			(int) Math.round(tile_width * 1/e.cameraDistance()), 
+			(int) Math.round(tile_height * 1/e.cameraDistance()), null);	
 		    } 
 		    k1++;
 		} 
